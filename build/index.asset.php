bc47dbc86b417c00d25638628e2f1e36ef3bc37e
Anh Phi Nguyen <nguyenanhphi077@gmail.com>
2026-05-07 10:06:44 +0700

    css

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'd2af0eeb49e47ffd7ae7');
